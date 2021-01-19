d39cbb4fb52bba5f0a3762f07036eb1cb574f3bf
Afonso -aptsharp <aff-on@hotmail.com>
2021-01-19 13:18:55 -0300

    Ajustes da configuração da action

--- src/app/pages/home/state/home.reducer.ts
+++ src/app/pages/home/state/home.reducer.ts
@@ -4,11 +4,13 @@ import * as fromHomeActions from './home.actions';
 
 export interface HomeState{
     text: string;
+    text2: string;
 
 }
 
 export const homeInitialState: HomeState ={
-    text: 'Porto Alegre'
+    text: 'Porto Alegre',
+    text2: 'QWE',
 
 }
 
